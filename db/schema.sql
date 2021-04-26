9c0d649c13d010819411ea457759c85da69067a0
Lacey <lacey.griffith@g.austincc.edu>
2021-04-26 15:27:07 -0500

    set up database and employee table

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+ CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role VARCHAR(30) NOT NULL,
+    department VARCHAR(40) NOT NULL,
+    manager VARCHAR(30) NOT NULL,
+    salary INT
+    );
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name, role, department, manager, salary)
+VALUES
+('Vicki','Trevors','cashier','sales','John',20000);
