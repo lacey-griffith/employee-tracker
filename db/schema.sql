f4f2c9b731432dc05f573ad584433ee4bb082436
Lacey <lacey.griffith@g.austincc.edu>
2021-04-27 10:34:45 -0500

    install npm packages

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,6 @@
     last_name VARCHAR(30) NOT NULL,
     role VARCHAR(30) NOT NULL,
     department VARCHAR(40) NOT NULL,
-    manager VARCHAR(30) NOT NULL,
+    manager VARCHAR(30),
     salary INT
     );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO employees (first_name, last_name, role, department, manager, salary)
 VALUES
-('Vicki','Trevors','cashier','sales','John',20000);
+('Vicki','Trevors','cashier','sales','John Miller',20000),
+('John','Miller','manager','sales','Polly Martin', 45000);
